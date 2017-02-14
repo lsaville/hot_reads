9285736fe10ff4543a552addceeaed3398c0befe
Lee Saville <l.w.saville@gmail.com>
2017-02-13 19:01:19 -0700

    Add double quotes to string

--- spec/requests/api/v1/reads_endpoint_spec.rb
+++ spec/requests/api/v1/reads_endpoint_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'POST to /api/v1/reads' do
   it 'exists' do
-    data = { read: 'https://www.youtube.com/watch?v=tntOCGkgt98'}
-
+    data = { read: "https://www.youtube.com/watch?v=tntOCGkgt98"}
+binding.pry
     post "/api/v1/reads", data.to_json
 
     expect(response).to be_success
