bba7a58742772a901e68286053d1a79456ba8218
Lee Saville <l.w.saville@gmail.com>
2017-02-13 19:42:27 -0700

    Add expectation about Read.count increasing indicating read creation

--- spec/requests/api/v1/reads_endpoint_spec.rb
+++ spec/requests/api/v1/reads_endpoint_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 describe 'POST to /api/v1/reads' do
   it 'exists' do
     data = { read: "https://www.youtube.com/watch?v=tntOCGkgt98"}
-binding.pry
+
+    read_count = Read.count
+
     post "/api/v1/reads", data.to_json
 
     expect(response).to be_success
+    expect(Read.count).to eq(read_count + 1)
   end
 end
