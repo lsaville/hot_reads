edb935bbd5ce347830d7b1769c3357a31ef5915d
Lee Saville <l.w.saville@gmail.com>
2017-02-13 19:00:23 -0700

    Add logic for the creation of a read

--- app/controllers/api/v1/reads_controller.rb
+++ app/controllers/api/v1/reads_controller.rb
@@ -2,5 +2,7 @@ class Api::V1::ReadsController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
+    read = Read.new(url: params['read'])
+    read.save
   end
 end
