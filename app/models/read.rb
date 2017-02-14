591efa8b4f39992328e96b190db59186e8010d93
Lee Saville <l.w.saville@gmail.com>
2017-02-13 19:41:20 -0700

    Add model class method to get hot reads to display in the index

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,2 +1,10 @@
 class Read < ApplicationRecord
+  def self.top_ten_from_last_24
+    results = where('created_at > ?', 24.hours.ago)
+      .group('url')
+      .limit(10)
+      .count
+    ordered = Hash[results.sort_by { |key, value| value }.reverse]
+    ordered.keys
+  end
 end
